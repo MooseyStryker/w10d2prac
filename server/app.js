2952f24c777aac6effc46d2f22da7f6d7fe2191d
Stryker Huber <stryker.important@gmail.com>
2023-12-05 16:05:55 -0700

    First push

--- server/app.js
+++ server/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const logger = (req,res,next) => {
+  console.log(`${req.method} has made a request for ${req.path}`);
+  res.on('finish', () => {
+    console.log(`${res.statusCode} is the status code`)
+  })
+  next()
+}
+
 // For testing purposes, GET /
-app.get('/', (req, res) => {
+app.get('/', logger, (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
 });
 
 // For testing express.json middleware
-app.post('/test-json', (req, res, next) => {
+app.post('/test-json', logger, (req, res, next) => {
   // send the body as JSON with a Content-Type header of "application/json"
   // finishes the response, res.end()
   res.json(req.body);
@@ -18,6 +28,21 @@ app.post('/test-json', (req, res, next) => {
 app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
 });
+
+app.use((req,res,next) => {
+  const noMethodFound = new Error("The requested resource couldn't be found.");
+  noMethodFound.statusCode = 404;
+  next(noMethodFound);
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode
+  })
+})
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
